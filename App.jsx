61a8f53918dabdab49f144bf7cc69c9151c0e971
ferdia77 <kingqueensuited22@gmail.com>
2025-02-07 12:52:03 +0100

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
 import Nav from "./components/Nav.jsx"
 import Users from "./pages/Users.jsx";
 
